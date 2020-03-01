609c91ba1d36c3ec4aa0319662dd5313f4be4a6a
Gregory Brutsche <gregory@mail.brutsches.local>
2020-02-29 19:42:17 -0500

    Move evwp version into a separate exe.

---
+++ MHW-Weapon-Hider-EVWP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace MHW_Weapon_Hider_EVWP {
+    public static class Program {
+        [STAThread]
+        public static void Main(string[] args) {
+            if (args.Length < 1) return;
+            var target = args[0];
+
+            var mod3Files = Directory.EnumerateFiles(target, "*.evwp", SearchOption.AllDirectories);
+
+            foreach (var file in mod3Files) {
+                ParseEvwp(file);
+            }
+        }
+
+        public static void ParseEvwp(string targetFile) { // ^\\wp\\.+\.evwp$
+            using var writer = new BinaryWriter(new FileStream(targetFile, FileMode.Open, FileAccess.ReadWrite));
+            var stream = writer.BaseStream;
+            // x: 68, y: 72, z: 76
+            stream.Seek(72, SeekOrigin.Begin);
+            writer.Write(-5000f);
+        }
+    }
+}
